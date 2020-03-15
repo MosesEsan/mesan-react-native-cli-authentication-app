ecf69792da0896c0a34af51269007719a4d30df8
Moses Esan <esanm@rte.ie>
2020-03-15 21:20:35 +0000

    Added Components

---
+++ app/components/Shared.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Icon, Badge} from 'react-native-elements';
+
+//HEADER COMPONENT
+export const Header = (props) => {
+    let {title, style} = props;
+
+    return (
+        <View style={[styles.header, style]}>
+            <Text style={styles.headerText}>
+                {title}
+            </Text>
+        </View>
+    )
+};
+
+Header.defaultProps = {
+    title: "",
+    style: {}
+};
+
+//ERROR COMPONENT
+export const ErrorText = ({error}) => {
+    return <Text style={styles.errorText}>{error}</Text>
+};
+
+ErrorText.defaultProps = {
+    error: ""
+};
+
+const styles = StyleSheet.create({
+    header: {
+        height: 50,
+        justifyContent: "center"
+    },
+
+    headerText: {
+        fontSize: 25,
+        color: "#362068",
+        fontWeight: "400",
+        fontFamily: "Helvetica Neue"
+    },
+
+    errorText:{
+        marginBottom: 8,
+        color:"red"
+    }
+});
