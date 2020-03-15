64a83b6f0f1a0a91997bc89b98318944fd1560df
Moses Esan <esanm@rte.ie>
2020-03-15 21:21:03 +0000

    Added Authentication Scenes

---
+++ app/scenes/auth/Username.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {View} from 'react-native';
+
+import * as api from "../../services/auth";
+import { useAuth } from "../../provider";
+
+import Form from 'react-native-basic-form';
+import {Header, ErrorText} from "../../components/Shared";
+
+export default function Username (props) {
+    const {navigation} = props;
+
+    //1 - DECLARE VARIABLES
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const { state, updateUser } = useAuth();
+
+    const fields = [
+        {name: 'username', label: 'Username', required: true}
+    ];
+
+    async function onSubmit(data) {
+        setLoading(true);
+
+        try {
+            let response = await api.updateProfile(state.user._id, data);
+            updateUser(response.user);
+
+            setLoading(false);
+
+            navigation.navigate('App');
+        } catch (error) {
+            setError(error.message);
+            setLoading(false)
+        }
+    }
+
+    let formProps = {title: "Submit", fields, onSubmit, loading };
+    return (
+        <View style={{flex: 1, paddingHorizontal: 16, backgroundColor:"#fff"}}>
+            <Header title={"Select Username"}/>
+            <View style={{flex:1}}>
+                <ErrorText error={error}/>
+                <Form {...formProps}/>
+            </View>
+        </View>
+    );
+};
+
+Username.navigationOptions = ({}) => {
+    return {
+        title: ``
+    }
+};
